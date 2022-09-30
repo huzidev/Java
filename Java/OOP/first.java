28c58ce5f019f2e58708d5e3128dda7c740d1c24
huzidev <huzaifa.iqdev@gmail.com>
2022-09-30 10:29:00 +0500

    names fix

---
+++ Java/OOP/first.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class first {
+    public static void main(String args[]) {
+        System.out.println("Hello, world");
+        int a;
+        System.out.print("Enter Number: ");
+        Scanner s = new Scanner(System.in);
+        a = s.nextInt();
+        System.out.println(a);
+    }
+}
