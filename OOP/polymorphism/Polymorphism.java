42f3434c01811d60f7102eff0c1af6be2049dad9
huzidev <huzaifa.iqdev@gmail.com>
2022-09-30 19:20:43 +0500

    basic structure for class

--- OOP/polymorphism/Polymorphism.java
+++ OOP/polymorphism/Polymorphism.java
@@ -1,3 +1,9 @@
+public class Student {
+    String name;
+    String hobby;
+    int age;
+}
+
 public class Polymorphism {
     public static void main(String[] args) {
         System.out.println("Hello, world");
