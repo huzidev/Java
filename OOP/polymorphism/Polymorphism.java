2e436f67991c63f6034b49edca7b729f9fe74270
huzidev <huzaifa.iqdev@gmail.com>
2022-09-30 16:27:40 +0500

    many functions

--- OOP/polymorphism/Polymorphism.java
+++ OOP/polymorphism/Polymorphism.java
@@ -1,7 +1,18 @@
 // classes starts with capital letter
 class Bike {
     String name;
+    String type;
     int model;
+
+    public void displayInfo(String name) {
+        System.out.println(name);
+    }
+    public void displayInfo(String type) {
+        System.out.println(type);
+    }
+    public void displayInfo(int model) {
+        System.out.println(model);
+    }
 }
 
 public class Polymorphism {
