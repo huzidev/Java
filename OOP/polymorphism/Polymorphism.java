b6e292703e84ec2c6f6ae839f833c3f61420ceed
huzidev <huzaifa.iqdev@gmail.com>
2022-09-30 16:51:39 +0500

    fixing errors

--- OOP/polymorphism/Polymorphism.java
+++ OOP/polymorphism/Polymorphism.java
@@ -3,20 +3,16 @@ class Bike {
     String name;
     String type;
     int model;
-    float avg;
 
     // diff fucntions with same name is called (FUCNTION OVERLOADING) using func with same NAME in a single class
     public void displayInfo(String name) {
         System.out.println(name);
     }
-    // can't pass parameters with String or int (MULRIPLE) times if using same name for for functions (FUCNTION OVERLOADING) rather pass mupltiple parameters like here String name and String type
-    public void displayInfo(String name, String type) {
-        System.out.println(name + " " + type);
+    // can't pass parameters with String or int (MULTIPLE) times if using same name for for functions (FUCNTION OVERLOADING) rather pass mupltiple parameters like here String type and int model
+    public void displayInfo(String type, int model) {
+        System.out.println(type + " " + model);
     }
     public void displayInfo(int model) {
-        System.out.println(model);
-    }
-    public void displayInfo(float avg) {
         System.out.println(model);
     }
 }
@@ -29,13 +25,12 @@ public class Polymorphism {
         petrolBike.name = "Unique";
         petrolBike.type = "Petrol";
         petrolBike.model = 2022;
-        petrolBike.avg = 60.5;
         
         elecBike.name = "Honda";
         elecBike.type = "Electric";
         elecBike.model = 2022;
-        elecBike.avg = 100.5;
 
-        petrolBike.displayInfo(petrolBike.name, petrolBike.type, petrolBike.model, petrolBike.avg)
+        petrolBike.displayInfo(petrolBike.name, petrolBike.type, petrolBike.model);
+        elecBike.displayInfo(elecBike.name, elecBike.type, elecBike.model);
     }
 }
