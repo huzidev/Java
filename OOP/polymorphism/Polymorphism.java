cc082ff41518918a882d9efab05b2b36fd4a068b
huzidev <huzaifa.iqdev@gmail.com>
2022-09-30 19:34:00 +0500

    polumorphism completed

--- OOP/polymorphism/Polymorphism.java
+++ OOP/polymorphism/Polymorphism.java
@@ -12,10 +12,17 @@ public class Polymorphism {
     public static void main(String[] args) {
         System.out.println("Hello, world");
         Bike bikeA = new Bike();
+        Bike bikeB = new Bike();
 
         bikeA.name = "Unique";
         bikeA.model = 2022;
         bikeA.type = "Electric Bike";
+        
+        bikeB.name = "Honda";
+        bikeB.model = 2022;
+        bikeB.type = "Petrol Bike";
+
         bikeA.printInfo(bikeA.name, bikeA.model, bikeA.type);
+        bikeB.printInfo(bikeB.name, bikeB.model, bikeB.type);
     }
 }
