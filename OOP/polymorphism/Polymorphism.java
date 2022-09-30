cd4fb4ad747e10dcc63178970529ec41ea60c346
huzidev <huzaifa.iqdev@gmail.com>
2022-09-30 19:21:30 +0500

    coding for basic oops

--- OOP/polymorphism/Polymorphism.java
+++ OOP/polymorphism/Polymorphism.java
@@ -1,11 +1,16 @@
-public class Student {
+class Student {
     String name;
     String hobby;
     int age;
+    
+    public void printInfo(String name) {
+        System.out.println(name);
+    }
 }
 
 public class Polymorphism {
     public static void main(String[] args) {
         System.out.println("Hello, world");
+        
     }
 }
