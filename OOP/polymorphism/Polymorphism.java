34b00e88e93f99eeebdee8d52ec0a80cc8e578aa
huzidev <huzaifa.iqdev@gmail.com>
2022-09-30 19:29:08 +0500

    errors fix

--- OOP/polymorphism/Polymorphism.java
+++ OOP/polymorphism/Polymorphism.java
@@ -1,16 +1,25 @@
-class Student {
+class Bike {
     String name;
-    String hobby;
-    int age;
+    int model;
     
     public void printInfo(String name) {
         System.out.println(name);
+    }
+    public void printInfo(int model) {
+        System.out.println(model);
+    }
+    public void printInfo(String name, int model) {
+        System.out.println(name + " is of year " + model);
     }
 }
 
 public class Polymorphism {
     public static void main(String[] args) {
         System.out.println("Hello, world");
-        
+        Bike bikeA = new Bike();
+
+        bikeA.name = "Unique";
+        bikeA.model = 2022;
+        bikeA.printInfo(bikeA.name, bikeA.model);
     }
 }
