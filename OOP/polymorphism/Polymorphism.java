0d338c8f1140504ad2802ed2c3fc5e2f866d8372
huzidev <huzaifa.iqdev@gmail.com>
2022-09-30 19:19:34 +0500

    debugging code

--- OOP/polymorphism/Polymorphism.java
+++ OOP/polymorphism/Polymorphism.java
@@ -1,36 +1,5 @@
-// classes starts with capital letter
-class Bike {
-    String name;
-    String type;
-    int model;
-
-    // diff fucntions with same name is called (FUCNTION OVERLOADING) using func with same NAME in a single class
-    public void displayInfo(String name) {
-        System.out.println(name);
-    }
-    // can't pass parameters with String or int (MULTIPLE) times if using same name for for functions (FUCNTION OVERLOADING) rather pass mupltiple parameters like here String type and int model
-    public void displayInfo(String type, int model) {
-        System.out.println(type + " " + model);
-    }
-    public void displayInfo(int model) {
-        System.out.println(model);
-    }
-}
-
 public class Polymorphism {
     public static void main(String[] args) {
-        Bike petrolBike = new Bike();
-        Bike elecBike = new Bike();
-
-        petrolBike.name = "Unique";
-        petrolBike.type = "Petrol";
-        petrolBike.model = 2022;
-        
-        elecBike.name = "Honda";
-        elecBike.type = "Electric";
-        elecBike.model = 2022;
-
-        petrolBike.displayInfo(petrolBike.name, petrolBike.type, petrolBike.model);
-        elecBike.displayInfo(elecBike.name, elecBike.type, elecBike.model);
+        System.out.println("Hello, world");
     }
 }
