00e8314671f7f90ff31532590da560227351e6a7
huzidev <huzaifa.iqdev@gmail.com>
2022-10-01 11:34:09 +0500

    labels text added

--- GUI/First/Test.java
+++ GUI/First/Test.java
@@ -9,14 +9,15 @@ public class Test {
         JLabel text = new JLabel();
 
         // settting text for frame
-        text.setText("Hello");
+        text.setText("Hello, World!");
 
         // setting frame
         frame.setTitle("HuziDev GUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
         frame.setSize(1080, 720);
         frame.setVisible(true);
+        frame.setResizable(false);
+        frame.add(text);
 
         // setting Icon (Favicon)
         ImageIcon icon = new ImageIcon("linux.png");
