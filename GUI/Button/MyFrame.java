50dc43ee356fdae04ec5e85db25a076c42c2a9bf
huzidev <huzaifa.iqdev@gmail.com>
2022-10-01 15:52:29 +0500

    boundaries for button

--- GUI/Button/MyFrame.java
+++ GUI/Button/MyFrame.java
@@ -6,11 +6,13 @@ import javax.swing.JButton;
 public class MyFrame extends JFrame {
     MyFrame() {
         JButton button = new JButton();
+        button.setBounds(250, 100, 100, 50);
 
         this.setTitle("HuziDev GUI");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(1080, 720);
         this.setVisible(true);
         this.setResizable(false);
+        this.add(button)
     }
 }
