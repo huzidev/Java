9cd4f90b78c8abb0a69c08a973efe53195b57065
huzidev <huzaifa.iqdev@gmail.com>
2022-10-01 15:48:57 +0500

    OOPS concepts for inheritance

--- GUI/Button/MyFrame.java
+++ GUI/Button/MyFrame.java
@@ -3,5 +3,11 @@ import javax.swing.JFrame;
 import javax.swing.JFrame;
 
 public class MyFrame extends JFrame {
-    
+    MyFrame() {
+        this.setTitle("HuziDev GUI");
+        this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+        this.setSize(1080, 720);
+        this.setVisible(true);
+        this.setResizable(false);
+    }
 }
