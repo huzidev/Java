763751f271efaa88610bc3bcd32e1596661770eb
huzidev <huzaifa.iqdev@gmail.com>
2022-10-01 16:10:02 +0500

    lambda expression

--- GUI/Button/MyFrame.java
+++ GUI/Button/MyFrame.java
@@ -1,16 +1,14 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 
-public class MyFrame extends JFrame implements ActionListener {
+public class MyFrame extends JFrame {
     // setting button as global so it can be used in actionPerfomed function
     JButton button;
     MyFrame() {
         button = new JButton();
         // boundary for buttons
         button.setBounds(250, 100, 100, 50);
-        button.addActionListener(this);
+        button.addActionListener(e -> System.out.println("Button Clicked"));
 
         this.setTitle("HuziDev GUI");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -18,14 +16,5 @@ public class MyFrame extends JFrame implements ActionListener {
         this.setVisible(true);
         this.setResizable(false);
         this.add(button);
-    }
-
-    // ActionListener for button and @Override public void... will be written automatically
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // means if action performed is button
-        if (e.getSource() == button) {
-            System.out.println("Button clicked");
-        }
     }
 }
