e9bf45a2bdf0a299a8b5d329ab3aca23d40bc54c
huzidev <huzaifa.iqdev@gmail.com>
2022-10-01 15:49:37 +0500

    error sfix

--- GUI/Button/MyFrame.java
+++ GUI/Button/MyFrame.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class MyFrame extends JFrame {
     MyFrame() {
         this.setTitle("HuziDev GUI");
-        this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(1080, 720);
         this.setVisible(true);
         this.setResizable(false);
