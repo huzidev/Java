d9f9a7d49c55af4a5bdb35e6c2cd7c60ed103e14
huzidev <huzaifa.iqdev@gmail.com>
2022-10-01 17:09:28 +0500

    fixing image size

--- GUI/Button/MyFrame.java
+++ GUI/Button/MyFrame.java
@@ -4,15 +4,23 @@ import javax.swing.JButton;
 
 public class MyFrame extends JFrame {
     MyFrame() {
-        ImageIcon image = new ImageIcon();
+        ImageIcon image = new ImageIcon("linux.png");
         JButton button = new JButton();
+        
         // boundary for buttons
         button.setBounds(200, 100, 100, 50);
+
         // e -> is called lambda expression
         button.addActionListener(e -> System.out.println("Button Clicked"));
         button.setText("Button");
+
         // to get rid of unnecessary border after click on button
         button.setFocusable(false);
+
+        // button.setIcon so Image will appear inside button
+        button.setIcon(image);
+        button.setHorizontalTextPosition(JButton.CENTER);
+        button.setVerticalTextPosition(JButton.BOTTOM);
 
         this.setTitle("HuziDev GUI");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
