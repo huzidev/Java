3671f91aeac660ce235261140e83fbc56a305a5d
huzidev <huzaifa.iqdev@gmail.com>
2022-10-02 13:42:15 +0500

    boiler plate for frame

--- GUI/Border-layout/MyFrame.java
+++ GUI/Border-layout/MyFrame.java
@@ -3,5 +3,9 @@ import javax.swing.JFrame;
 public class MyFrame {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1080, 720);
+        frame.setVisible(true);
     }
 }
