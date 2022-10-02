cf86227aac03e99e525ecca6c15e06cdd82d237e
huzidev <huzaifa.iqdev@gmail.com>
2022-10-02 13:43:22 +0500

    j panels

--- GUI/Border-layout/MyFrame.java
+++ GUI/Border-layout/MyFrame.java
@@ -1,8 +1,16 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MyFrame {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
+
+        // panels are like div
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        JPanel panel4 = new JPanel();
+        JPanel panel5 = new JPanel();
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1080, 720);
