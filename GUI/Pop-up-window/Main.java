c244d53f7f711533c43231b43cd9fba5d16b5781
huzidev <huzaifa.iqdev@gmail.com>
2022-10-03 18:58:29 +0500

    question message

--- GUI/Pop-up-window/Main.java
+++ GUI/Pop-up-window/Main.java
@@ -5,12 +5,18 @@ public class Main {
     public static void main(String[] args) {
 
         // this is plain messgae without any ICON
-        JOptionPane.showMessageDialog(null, "Hello, World!", "Simple Message", JOptionPane.PLAIN_MESSAGE);
+        // JOptionPane.showMessageDialog(null, "Hello, World!", "Simple Message", JOptionPane.PLAIN_MESSAGE);
         
         // informative message with Inforamtion ICON
         // JOptionPane.showMessageDialog(null, "You are learning basics of Java", "Learning", JOptionPane.INFORMATION_MESSAGE);
+        
+        // Question message
+        JOptionPane.showMessageDialog(null, "How Are You?", "Question", JOptionPane.QUESTION_MESSAGE);
+        
+        
         // JOptionPane.showMessageDialog(null, "Hello, World!", "Simple Message", JOptionPane.PLAIN_MESSAGE);
-        // JOptionPane.showMessageDialog(null, "Hello, World!", "Simple Message", JOptionPane.PLAIN_MESSAGE);
+        
+        
         // JOptionPane.showMessageDialog(null, "Hello, World!", "Simple Message", JOptionPane.PLAIN_MESSAGE);
     }
 }
