f2ec1dbb2c15ec12a514be69cd83f1a93df9c594
huzidev <huzaifa.iqdev@gmail.com>
2022-10-03 10:18:13 +0500

    size frame

--- GUI/Flowlayout/MyFrame.java
+++ GUI/Flowlayout/MyFrame.java
@@ -4,6 +4,6 @@ public class MyFrame {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+        frame.setSize(1080, 720);
     }
 }
