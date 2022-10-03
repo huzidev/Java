4cbc7ebe500f0dd25441639f3e3dbfd93aa57b06
huzidev <huzaifa.iqdev@gmail.com>
2022-10-03 19:54:30 +0500

    implementing functions

--- GUI/True-False/MyFrame.java
+++ GUI/True-False/MyFrame.java
@@ -2,7 +2,7 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
 
-public class MyFrame {
+public class MyFrame implements {
     
     JFrame frame = new JFrame();
     public static void main(String[] args) {
