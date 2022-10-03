85b41bc05bd0f86a5cff848f00cde091a216a9cc
huzidev <huzaifa.iqdev@gmail.com>
2022-10-03 16:50:47 +0500

    button for new page

--- GUI/New-Window/LaunchPage.java
+++ GUI/New-Window/LaunchPage.java
@@ -1,7 +1,12 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class LaunchPage {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame();
-    }
+
+    JFrame frame = new JFrame();
+    JButton button = new JButton();
+
+   LaunchPage() {
+
+   }
 }
