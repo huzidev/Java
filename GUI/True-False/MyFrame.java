42983bf4982aedd85641b51926bd8a1cd5c15df5
huzidev <huzaifa.iqdev@gmail.com>
2022-10-03 21:00:28 +0500

    checkbox with java

--- GUI/True-False/MyFrame.java
+++ GUI/True-False/MyFrame.java
@@ -6,6 +6,7 @@ public class MyFrame implements ActionListener {
     
     public static void main(String[] args) {
         JFrame frame = new JFrame();
+        JCheckBox checkBox = new JCheckBox();
         
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new FlowLayout());
