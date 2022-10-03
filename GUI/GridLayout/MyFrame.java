ffd01370c00488970b7e106cef3d42ab8d6c813d
huzidev <huzaifa.iqdev@gmail.com>
2022-10-03 16:32:56 +0500

    adding button java

--- GUI/GridLayout/MyFrame.java
+++ GUI/GridLayout/MyFrame.java
@@ -1,4 +1,6 @@
 import java.awt.GridLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 // GRID-LAYOUT wil 3x3 means 3 columns and 3 rows
@@ -10,6 +12,7 @@ public class MyFrame {
         frame.setSize(1080, 720);
         frame.setLayout(new GridLayout(3, 3, 10, 10)); // 3rd and 4th are magin
 
+        frame.add(new JButton("1"));
         frame.setVisible(true);
     }
 }
