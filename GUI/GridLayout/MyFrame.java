6f5feb44e71376c1690ef020b337fa29cb57bb68
huzidev <huzaifa.iqdev@gmail.com>
2022-10-03 16:33:34 +0500

    grid buttons added

--- GUI/GridLayout/MyFrame.java
+++ GUI/GridLayout/MyFrame.java
@@ -13,6 +13,14 @@ public class MyFrame {
         frame.setLayout(new GridLayout(3, 3, 10, 10)); // 3rd and 4th are magin
 
         frame.add(new JButton("1"));
+        frame.add(new JButton("2"));
+        frame.add(new JButton("3"));
+        frame.add(new JButton("4"));
+        frame.add(new JButton("5"));
+        frame.add(new JButton("6"));
+        frame.add(new JButton("7"));
+        frame.add(new JButton("8"));
+        frame.add(new JButton("9"));
         frame.setVisible(true);
     }
 }
