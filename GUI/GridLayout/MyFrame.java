550e1e8841a71d58431dd2b875cdd6e8191f4306
huzidev <huzaifa.iqdev@gmail.com>
2022-10-03 16:31:59 +0500

    grid layout

--- GUI/GridLayout/MyFrame.java
+++ GUI/GridLayout/MyFrame.java
@@ -1,9 +1,15 @@
+import java.awt.GridLayout;
 import javax.swing.JFrame;
 
+// GRID-LAYOUT wil 3x3 means 3 columns and 3 rows
 public class MyFrame {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1080, 720);
+        frame.setLayout(new GridLayout());
+
+        frame.setVisible(true);
     }
 }
