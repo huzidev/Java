17c82e12277896212c46404b429be8b45f0e7667
huzidev <huzaifa.iqdev@gmail.com>
2022-10-03 21:18:16 +0500

    adding images as icon

--- GUI/True-False/MyFrame.java
+++ GUI/True-False/MyFrame.java
@@ -5,15 +5,23 @@ import java.awt.*;
 public class MyFrame implements ActionListener {
     JButton button;
     JCheckBox checkBox;
+    ImageIcon fIcon;
+    ImageIcon tIcon;
+
     MyFrame() {
         JFrame frame = new JFrame();
+
+        fIcon = new ImageIcon("linux.png");
+        tIcon = new ImageIcon("linuxx.jpg");
+
         checkBox = new JCheckBox("I'm not a robot");
 
         button = new JButton("Submit");
         button.addActionListener(this);
 
         checkBox.setFocusable(false);
-        checkBox.setFont(new Font(null, Font.PLAIN, 25));        
+        checkBox.setFont(new Font(null, Font.PLAIN, 25));
+        checkIcon.setIcon();   
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1080, 720);
