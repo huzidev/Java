ad1817532f3fd3f96d9fc4325e795203a6004f6e
huzidev <huzaifa.iqdev@gmail.com>
2022-10-03 10:28:55 +0500

    buttons for panel

--- GUI/Flowlayout/MyFrame.java
+++ GUI/Flowlayout/MyFrame.java
@@ -20,16 +20,17 @@ public class MyFrame {
         panel.setPreferredSize(new Dimension(500, 700));
         panel.setBackground(Color.BLACK);
         
-        frame.add(new JButton("1"));
-        frame.add(new JButton("2"));
-        frame.add(new JButton("3"));
-        frame.add(new JButton("4"));
-        frame.add(new JButton("5"));
-        frame.add(new JButton("6"));
-        frame.add(new JButton("7"));
-        frame.add(new JButton("8"));
-        frame.add(new JButton("9"));
-        frame.add(new JButton("10"));
+        // inseting button inside panel
+        panel.add(new JButton("1"));
+        panel.add(new JButton("2"));
+        panel.add(new JButton("3"));
+        panel.add(new JButton("4"));
+        panel.add(new JButton("5"));
+        panel.add(new JButton("6"));
+        panel.add(new JButton("7"));
+        panel.add(new JButton("8"));
+        panel.add(new JButton("9"));
+        panel.add(new JButton("10"));
 
         frame.add(panel);
     }
