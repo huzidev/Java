9caed7a4ea5b4fe9e4fbec7bab2ad2b313bf9c97
huzidev <huzaifa.iqdev@gmail.com>
2022-10-03 21:02:40 +0500

    text with check-box

--- GUI/True-False/MyFrame.java
+++ GUI/True-False/MyFrame.java
@@ -7,7 +7,9 @@ public class MyFrame implements ActionListener {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         JCheckBox checkBox = new JCheckBox();
-        
+
+        checkBox.setText("I'm not a robot");
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1080, 720);
         frame.setLayout(new FlowLayout());
