9162ce9fda9c364d1da1b80e72fc9b1bb628fe47
huzidev <huzaifa.iqdev@gmail.com>
2022-10-03 18:57:31 +0500

    icons diff

--- GUI/Pop-up-window/Main.java
+++ GUI/Pop-up-window/Main.java
@@ -3,6 +3,14 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+
+        // this is plain messgae without any ICON
         JOptionPane.showMessageDialog(null, "Hello, World!", "Simple Message", JOptionPane.PLAIN_MESSAGE);
+        
+        // informative message with Inforamtion ICON
+        // JOptionPane.showMessageDialog(null, "You are learning basics of Java", "Learning", JOptionPane.INFORMATION_MESSAGE);
+        // JOptionPane.showMessageDialog(null, "Hello, World!", "Simple Message", JOptionPane.PLAIN_MESSAGE);
+        // JOptionPane.showMessageDialog(null, "Hello, World!", "Simple Message", JOptionPane.PLAIN_MESSAGE);
+        // JOptionPane.showMessageDialog(null, "Hello, World!", "Simple Message", JOptionPane.PLAIN_MESSAGE);
     }
 }
