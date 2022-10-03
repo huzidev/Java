c02d261bc87a0e859739c0b1955772df0cb0c844
huzidev <huzaifa.iqdev@gmail.com>
2022-10-03 19:54:57 +0500

    fixing if conditions

--- GUI/True-False/MyFrame.java
+++ GUI/True-False/MyFrame.java
@@ -2,7 +2,7 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
 
-public class MyFrame implements {
+public class MyFrame implements ActionListener {
     
     JFrame frame = new JFrame();
     public static void main(String[] args) {
