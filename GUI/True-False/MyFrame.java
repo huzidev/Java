0e0ce846f79f66c86a208a85217b3d5a800743fb
huzidev <huzaifa.iqdev@gmail.com>
2022-10-03 21:01:41 +0500

    frame size

--- GUI/True-False/MyFrame.java
+++ GUI/True-False/MyFrame.java
@@ -9,6 +9,7 @@ public class MyFrame implements ActionListener {
         JCheckBox checkBox = new JCheckBox();
         
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1080, 720);
         frame.setLayout(new FlowLayout());
 
         frame.add(checkBox);
