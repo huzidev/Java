666966905e4a0f2d19b90125a285d9da77e0290d
huzidev <huzaifa.iqdev@gmail.com>
2022-10-03 17:03:40 +0500

    jlabel

--- GUI/New-Window/NewWindow.java
+++ GUI/New-Window/NewWindow.java
@@ -1,5 +1,17 @@
+import javax.swing.JLabel;
+
 public class NewWindow {
     public static void main(String[] args) {
         
+        JFrame frame = new JFrame();
+        JLabel text = new JLabel();
+
+        NewWindow() {
+            frame.setSize(1080, 720);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setLayout(null);
+            
+        }
+
     }
 }
