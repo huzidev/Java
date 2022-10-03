49258bcfa3a4446c48a8c8545bf51e972f659e49
huzidev <huzaifa.iqdev@gmail.com>
2022-10-03 18:55:55 +0500

    simple dialouge box

--- GUI/Pop-up-window/Main.java
+++ GUI/Pop-up-window/Main.java
@@ -1,5 +1,8 @@
+import java.awt.*;
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        
+        JOptionPane.showMessageDialog(null, "Hello, World!", "Simple Message", JOptionPane.PLAIN_MESSAGE);
     }
 }
