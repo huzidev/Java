cae6a7b957883689f7bb974c27d2a5e0dea7ae5e
huzidev <huzaifa.iqdev@gmail.com>
2022-10-03 10:19:44 +0500

    buttons created

--- GUI/Flowlayout/MyFrame.java
+++ GUI/Flowlayout/MyFrame.java
@@ -1,10 +1,23 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MyFrame {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1080, 720);
         frame.setVisible(true);
+
+        frame button = new JButton("1");
+        frame button = new JButton("2");
+        frame button = new JButton("3");
+        frame button = new JButton("4");
+        frame button = new JButton("5");
+        frame button = new JButton("6");
+        frame button = new JButton("7");
+        frame button = new JButton("8");
+        frame button = new JButton("9");
+        frame button = new JButton("10");
     }
 }
