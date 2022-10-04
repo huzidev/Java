c6e2dc6a8f0cfc56124e75a89145212863a83d83
huzidev <huzaifa.iqdev@gmail.com>
2022-10-04 15:27:10 +0500

    adding radio buttons

--- GUI/Radio-Buttons/MyFrame.java
+++ GUI/Radio-Buttons/MyFrame.java
@@ -7,9 +7,9 @@ public class MyFrame {
 
     MyFrame() {
         JFrame frame = new JFrame();
-        JRadioButton pizza = new JRadioButton();
-        JRadioButton burger = new JRadioButton();
-        JRadioButton pasta = new JRadioButton();
+        JRadioButton pizza = new JRadioButton("pizza");
+        JRadioButton burger = new JRadioButton("burger");
+        JRadioButton pasta = new JRadioButton("pasta");
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1080, 720);
