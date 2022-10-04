25d42761bcdac77dcdda77cdfb3bf7a92708c66b
huzidev <huzaifa.iqdev@gmail.com>
2022-10-04 15:31:03 +0500

    adding button group

--- GUI/Radio-Buttons/MyFrame.java
+++ GUI/Radio-Buttons/MyFrame.java
@@ -11,6 +11,8 @@ public class MyFrame implements ActionListener {
         JRadioButton burger = new JRadioButton("burger");
         JRadioButton pasta = new JRadioButton("pasta");
 
+        ButtonGroup group = new ButtonGroup();
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1080, 720);
         frame.setLayout(new FlowLayout());
