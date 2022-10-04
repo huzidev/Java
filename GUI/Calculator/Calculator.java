4262ae6d0cf93bbb9c17bc45362aeb74b58dea4d
huzidev <huzaifa.iqdev@gmail.com>
2022-10-04 21:52:33 +0500

    for loop for action listener

--- GUI/Calculator/Calculator.java
+++ GUI/Calculator/Calculator.java
@@ -109,6 +109,8 @@ public class Calculator implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+        for(int i = 0; i < 10; i++) {
+            
+        }   
     }
 }
