ce364479950c165f5a685f39d3c76bec61998ba7
huzidev <huzaifa.iqdev@gmail.com>
2022-10-04 18:24:58 +0500

    boiler palte

--- GUI/Animation/MyFrame.java
+++ GUI/Animation/MyFrame.java
@@ -7,6 +7,12 @@ public class MyFrame {
     MyPanel panel;
 
     MyFrame() {
+        JFrame frame = new JFrame();
         panel = new MyPanel();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1080, 720);
+        frame.setVisible(true);
+
     }
 }
