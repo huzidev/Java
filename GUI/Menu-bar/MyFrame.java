df0b6b259e982dab05460894bb9566bc9e125e49
huzidev <huzaifa.iqdev@gmail.com>
2022-10-04 17:39:30 +0500

    menu bar

--- GUI/Menu-bar/MyFrame.java
+++ GUI/Menu-bar/MyFrame.java
@@ -6,6 +6,14 @@ public class MyFrame implements ActionListener {
     MyFrame() {
         JFrame frame = new JFrame();
 
+        // for menu-bar
+        JMenuBar menuBar = new JMenu();
+
+        JMenu file = new JMenu();
+        JMenu file = new JMenu();
+        JMenu file = new JMenu();
+
+
         frame.setSize(1080, 720);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new FlowLayout());
