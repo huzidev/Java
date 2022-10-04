abaa0c4ec830b48f49e65883050f597725aed9ff
huzidev <huzaifa.iqdev@gmail.com>
2022-10-04 17:36:54 +0500

    flow layout frame java

--- GUI/Menu-bar/MyFrame.java
+++ GUI/Menu-bar/MyFrame.java
@@ -4,6 +4,12 @@ import java.awt.*;
 
 public class MyFrame {
     MyFrame() {
+        JFrame frame = new JFrame();
 
+        frame.setSize(1080, 720);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+        
+        frame.setVisible(true);
     }
 }
