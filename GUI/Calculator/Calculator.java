979204acf9452177cadc75942837bd1ada4a1fc3
huzidev <huzaifa.iqdev@gmail.com>
2022-10-04 21:07:09 +0500

    panel

--- GUI/Calculator/Calculator.java
+++ GUI/Calculator/Calculator.java
@@ -6,6 +6,7 @@ public class Calculator implements ActionListener {
 
     JFrame frame;
     JTextField textField;
+    
     // buttons for numbers 0 to 9
     JButton[] numberButtons = new JButton[10];
     JButton[] funcButtons = new JButton[8];
@@ -14,6 +15,7 @@ public class Calculator implements ActionListener {
     JPanel panel;
 
     double num1 = 0, num2 = 0, result = 0;
+    
     // for holding multiply, divide etc
     char operator;
 
