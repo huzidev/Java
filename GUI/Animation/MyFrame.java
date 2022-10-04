7bb2674ec1873cc22ba33521bdbd18184420a712
huzidev <huzaifa.iqdev@gmail.com>
2022-10-04 18:24:07 +0500

    global declearation

--- GUI/Animation/MyFrame.java
+++ GUI/Animation/MyFrame.java
@@ -1,5 +1,12 @@
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
+
 public class MyFrame {
+
+    MyPanel panel;
+
     MyFrame() {
-        
+        panel = new MyPanel();
     }
 }
