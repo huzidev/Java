491453c1671a81430474f700272c35c7727081e1
huzidev <huzaifa.iqdev@gmail.com>
2022-10-04 21:23:23 +0500

    for loop for buttons

--- GUI/Calculator/Calculator.java
+++ GUI/Calculator/Calculator.java
@@ -62,6 +62,10 @@ public class Calculator implements ActionListener {
             funcButtons[i].setFont(myFont);
         }
 
+        for(int i = 0; i < 10; i++) {
+            
+        }
+
         frame.add(textField);
         frame.setVisible(true);
             
